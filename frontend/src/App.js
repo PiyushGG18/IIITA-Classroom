740fbf899ca92ec24bbfef5424d650de1de6a1ea
Thakur Aman <thakuraman9191@gmail.com>
2024-02-05 00:07:36 +0530

    merge complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Login from "./components/login/Login";
+// import Login from "./components/login/Login";
 import UserCard from "./components/dashboard/UserCard";
+import Sidebarmerge from "./components/Sidebar/Sidebarmerge";
 
 function App() {
   return (
     <>
       {/* <Login/> */}
-      <UserCard
+      {/* <UserCard
         user="Roshan Chaudhary"
         userImage="/photos/Dashboard/dummy.jpeg"
         userEmail="chaudharyrhan@gmail.com"
-      />
+      /> */}
+      <Sidebarmerge/>
     </>
   );
 }
