44faf722cdbcd57d26274eab692d759dca97c264
Piyush Gawade <gawadepiyush007@gmail.com>
2024-04-21 15:22:07 +0530

    post upgraded

--- frontend/src/components/SubjectInfo/SubjectPost.jsx
+++ frontend/src/components/SubjectInfo/SubjectPost.jsx
@@ -1,5 +1,21 @@
 import React, { useContext } from "react";
 
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+
+  // Extracting date components
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  // Extracting time components
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes} ${day}/${month}/${year}`;
+}
+
 function SubjectPost(props) {
   // const { subjects } = useContext(UserContext);
   // const { subId } = useParams();
@@ -11,33 +27,37 @@ function SubjectPost(props) {
   const subPosts = props.sub.posts;
 
   const temp = subPosts ? [...subPosts].reverse() : [];
-  console.log(props.sub)
+  console.log(temp)
   return (
     <>
-      {temp.length > 0?
-      <div className="w-full flex flex-col items-center ">
-        {temp.map((post) => (
-          <div className="border border-gray-300 p-2 w-11/12 my-2 rounded-2xl flex flex-col">
-            <div className="p-2 flex">
-              <img
-                src={post.pfp}
-                alt="err"
-                className="w-12 rounded-full mr-4"
-              />
-              <div className="flex flex-col justify-center">
-                <div className="text-sm font-bold text-gray-600">
-                  {post.author}
+      {temp.length > 0 ? (
+        <div className="w-full flex flex-col items-center ">
+          {temp.map((post) => (
+            <div className="border border-gray-300 p-2 w-11/12 my-2 rounded-2xl flex flex-col">
+              <div className="p-2 flex">
+                <img
+                  src={post.authorImage}
+                  alt="err"
+                  className="w-12 rounded-full mr-4"
+                />
+                <div className="flex flex-col justify-center">
+                  <div className="text-sm font-bold text-gray-600">
+                    {post.author}
+                  </div>
+                  <div className="text-xs text-gray-400">{formatDate(post.date)}</div>
                 </div>
-                <div className="text-xs text-gray-400">{post.date}</div>
+              </div>
+              <div className="text-sm p-2 text-gray-700 overflow-auto">
+                {post.content}
               </div>
             </div>
-            <div className="text-sm p-2 text-gray-700 overflow-auto">
-              {post.content}
-            </div>
-          </div>
-        ))}
-      </div>
-      :<div className=" font-semibold text-gray-400  rounded-lg p-2 m-4 flex flex-col items-center">No posts yet</div>}
+          ))}
+        </div>
+      ) : (
+        <div className=" font-semibold text-gray-400  rounded-lg p-2 m-4 flex flex-col items-center">
+          No posts yet
+        </div>
+      )}
     </>
   );
 }
